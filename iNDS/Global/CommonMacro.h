865c4d9d87b2f9f587bca3a61650d6eb59cd4804
Shvier <daulvi@hotmail.com>
2018-02-27 14:09:48 +0800

    Fix navigationBackButton offset for iOS 11

---
+++ iNDS/Global/CommonMacro.h
@@ -0,0 +1,13 @@
+//
+//  CommonMacro.h
+//  iNDS
+//
+//  Created by Shvier on 27/02/2018.
+//  Copyright © 2018 iNDS. All rights reserved.
+//
+
+#ifndef CommonMacro_h
+#define CommonMacro_h
+
+
+#endif /* CommonMacro_h */
