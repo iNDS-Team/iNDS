e6271ebfddd7a99618ad6331891ff478201524bb
Frederick Morlock <FrederickGeek8@gmail.com>
2018-06-26 21:41:44 -0400

    Allow bluetooth routing

--- iNDS/iNDSEmulatorViewController.mm
+++ iNDS/iNDSEmulatorViewController.mm
@@ -350,7 +350,8 @@ enum VideoFilter : NSUInteger {
         // (Mute on && don't ignore it) or user has sound disabled
         BOOL muteSound = (muteDetector.isMute && ![defaults boolForKey:@"ignoreMute"]) || [defaults boolForKey:@"disableSound"];
         EMU_enableSound(!muteSound);
-        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+        AVAudioSessionCategoryOptions opts = AVAudioSessionCategoryOptionAllowBluetoothA2DP;
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:opts error:nil];
         
         // Filter
         int filterTranslate[] = {NONE, EPX, SUPEREAGLE, _2XSAI, SUPER2XSAI, BRZ2x, LQ2X, BRZ3x, HQ2X, HQ4X, BRZ4x, BRZ5x};
