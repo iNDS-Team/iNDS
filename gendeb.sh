a1daaeb917a97b35b2389e64630674fc9e75dd1c
Frederick Morlock <FrederickGeek8@gmail.com>
2018-06-27 23:59:16 -0400

    DEB Gen. Script

---
+++ gendeb.sh
@@ -0,0 +1,19 @@
+if ! which unipkg &> /dev/null; then
+    echo "unipkg is not installed. Install with npm install -g unipkg."
+    exit 1
+fi
+
+OUTDIR=./dist/out.xcarchive
+ORIG=$(pwd)
+xcodebuild -workspace iNDS.xcworkspace -scheme iNDS archive CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" -archivePath "$OUTDIR" | xcpretty
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    cd "$OUTDIR/Products"
+    INDS_VER=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Applications/iNDS.app/Info.plist)
+    cp -R $ORIG/DEBIAN .
+    ldid -S "Applications/iNDS.app/iNDS"
+    cd ..
+    INDS_VER=$INDS_VER unipkg build "Products"
+    cp "net.nerd.iNDS_${INDS_VER}_iphoneos-arm.deb" ../
+else
+    echo "\nBuild failed!"
+fi
