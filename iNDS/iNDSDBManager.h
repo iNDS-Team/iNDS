a1911a50369ab18ab9c3ab892e12c0acba79ac02
Frederick Morlock <FrederickGeek8@gmail.com>
2018-07-31 17:11:26 -0400

    Basic DB Manager

---
+++ iNDS/iNDSDBManager.h
@@ -0,0 +1,20 @@
+//
+//  iNDSDBManager.h
+//  iNDS
+//
+//  Created by Frederick Morlock on 7/31/18.
+//  Copyright © 2018 iNDS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface iNDSDBManager : NSObject {
+    sqlite3* _database;
+}
+
++ (id) sharedInstance;
+
+- (void) query:(NSString*)queryString result:(void (^)(int resultCode, sqlite3_stmt *statement))result;
+
+@end
---
+++ iNDS/iNDSDBManager.m
@@ -0,0 +1,51 @@
+//
+//  iNDSDBManager.m
+//  iNDS
+//
+//  Created by Frederick Morlock on 7/31/18.
+//  Copyright © 2018 iNDS. All rights reserved.
+//
+
+#import "iNDSDBManager.h"
+
+@implementation iNDSDBManager
+
++ (id)sharedInstance {
+    static iNDSDBManager *shared = nil;
+    static dispatch_once_t token;
+    dispatch_once(&token, ^{
+        shared = [[self alloc] init];
+    });
+    
+    return shared;
+}
+
+- (id) init {
+    if (self = [super init]) {
+        NSString *dbPath = [[NSBundle mainBundle] pathForResource:@"openvgdb" ofType:@"sqlite"];
+        
+        if (sqlite3_open([dbPath UTF8String], &_database) != SQLITE_OK) {
+            NSLog(@"Failed to open games database");
+        }
+    }
+    
+    return self;
+}
+
+- (void) query:(NSString*)queryString result:(void (^)(int resultCode, sqlite3_stmt *statement))result {
+    sqlite3_stmt *statement;
+    NSLog(@"%s", [queryString UTF8String]);
+    int resultCode = sqlite3_prepare_v2(_database, [queryString UTF8String], -1, &statement, nil);
+    
+    if (result) {
+        result(resultCode, statement);
+    }
+    
+//    sqlite3_finalize(statement);
+}
+
+- (void) dealloc {
+    sqlite3_close(_database);
+}
+
+@end
